71b00baf9f4922b58a76c63493ddd9b61d331dc9
tray0244 <tray024400@gmail.com>
2026-02-10 14:58:31 +0900

    fix: medium priority 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config: any) {
+  webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
