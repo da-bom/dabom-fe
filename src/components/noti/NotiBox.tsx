e47a1141016f0534cb2177f1bfed51d5d447ee73
tray0244 <tray024400@gmail.com>
2026-02-10 13:53:49 +0900

    refactor: NotiBox 피드백 수정

--- src/components/noti/NotiBox.tsx
+++ src/components/noti/NotiBox.tsx
@@ -1,31 +1,29 @@
-import { Badge } from "../common/Badge";
+import { Badge } from "@/components/common/Badge";
 import { cn } from "@/lib/utils";
 
 interface NotiBoxProps {
   title: string;
   description: string;
-  isNew?: boolean;
+  isRead?: boolean;
   className?: string;
 }
 
-export const NotiBox = ({ title, description, isNew = false, className }: NotiBoxProps) => {
+export const NotiBox = ({ title, description, isRead = true, className }: NotiBoxProps) => {
   return (
     <div
       className={cn(
-        "w-[calc(100%-46px)] max-w-[344px] h-[80px] mx-auto", 
-        
+        "w-full h-[80px] ",
         "flex flex-col justify-center px-5", 
         "bg-brand-white rounded-lg shadow-sm border transition-all",
         
-        isNew 
-          ? "border-primary-400 ring-1 ring-primary-400" 
+        !isRead 
+          ? "border-primary-400" 
           : "border-grayscale-200",
-        
         className
       )}
     >
-      <div className="flex items-center gap-2 mb-0.5">
-        {isNew && <Badge label="NEW" variant="default" />}
+      <div className="flex items-center gap-2">
+        {!isRead && <Badge variant="primary">NEW</Badge>}
         <h3 className="text-body1-m text-brand-black truncate">{title}</h3>
       </div>
       <p className="text-body2-m text-grayscale-600 line-clamp-1">{description}</p>
