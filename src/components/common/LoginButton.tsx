6e80013c682ee52f3feb468f97a327b526d65514
tray0244 <tray024400@gmail.com>
2026-02-10 17:27:00 +0900

    feat: 로그인 버튼 컴포넌트 구현

---
+++ src/components/common/LoginButton.tsx
@@ -0,0 +1,34 @@
+
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import { cn } from "@/lib/utils";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    children: ReactNode;
+    variant?: "primary" | "secondary";
+    fullWidth?: boolean;
+}
+
+export const Button = ({ 
+    children, 
+    className, 
+    variant = "primary", 
+    fullWidth = true,
+    ...props 
+}: ButtonProps) => {
+    return (
+    <button
+        className={cn(
+        "flex items-center justify-center rounded-[14px] px-6 py-4 transition-all active:scale-[0.98]",
+        "text-body1-m font-bold",
+        "disabled:bg-grayscale-300 disabled:cursor-not-allowed",
+        
+        variant === "primary" && "bg-brand-dark text-brand-white hover:bg-black", 
+        fullWidth ? "w-full" : "w-auto",
+        className
+        )}
+        {...props}
+    >
+        {children}
+    </button>
+    );
+};
