e05a8ae8bcd3e8b0b401efd2666c9b8ea5edc8db
tray0244 <tray024400@gmail.com>
2026-02-10 17:23:54 +0900

    feat: 로그인 화면 구현

---
+++ src/app/login/page.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import Header from '@/components/common/Header';
+import Input from '@/components/common/input';
+import { Button } from '@/components/common/LoginButton';
+
+export default function LoginPage() {
+    const router = useRouter();
+    
+    const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    router.push('/');
+};
+
+return (
+    <div className="min-h-screen bg-background-base">
+        <Header isBackVisible={true} className="bg-background-base" /> 
+        
+        <main className="px-5 pt-10">
+        <form onSubmit={handleLogin} className="flex flex-col gap-8">
+            <div className="flex flex-col gap-6">
+            <Input 
+                label="전화번호" 
+                type="tel" 
+                placeholder="전화번호를 입력해주세요" 
+            />
+
+            <Input 
+                label="비밀번호" 
+                type="password" 
+                placeholder="비밀번호를 입력해주세요" 
+            />
+            </div>
+                
+            <div className="mt-10">
+            <Button type="submit">
+                로그인
+            </Button>
+            </div>
+        </form>
+        </main>
+    </div>
+);
+}
