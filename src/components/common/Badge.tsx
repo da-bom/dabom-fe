cd91b6879362858ea932b266f2c2e36f24d99b85
tray0244 <tray024400@gmail.com>
2026-02-10 10:29:15 +0900

    refactor: Badge 타입 추가 구현

--- src/components/common/Badge.tsx
+++ src/components/common/Badge.tsx
@@ -1,23 +1,39 @@
 import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+type BadgeVariant = "default" | "white" | "gray" | "outline";
+type BadgeSize = "sm" | "lg";
 
 interface BadgeProps {
-  label: string;
+  children: ReactNode;
   className?: string;
-  variant?: "default"; 
+  variant?: BadgeVariant;
+  size?: BadgeSize;
 }
 
-export const Badge = ({ label, className, variant = "default" }: BadgeProps) => {
+export const Badge = ({ children, className, variant = "default", size = "sm" }: BadgeProps) => {
+  const variants: Record<BadgeVariant, string> = {
+    default: "bg-primary-400 text-brand-white border-transparent", 
+    white: "bg-brand-white text-primary-400 border-transparent",
+    gray: "bg-grayscale-200 border-transparent",
+    outline: "bg-brand-white text-primary-400 border-2 border-primary-400",
+  };
+
+  const sizes: Record<BadgeSize, string> = {
+    sm: "px-2.5 py-0.5 text-caption-d rounded-full h-[18px]",
+    lg: "px-4 py-1 text-caption-d rounded-full h-[32px]",
+  };
   return (
     <span
       className={cn(
         "inline-flex items-center justify-center shrink-0 whitespace-nowrap",
-        "rounded-full px-2.5 py-0.5",
-        "text-caption-m", 
-        variant === "default" && "bg-primary-400 text-brand-white",
+
+        sizes[size],
+        variants[variant],
         className
       )}
     >
-      {label}
+      {children}
     </span>
   );
 };
