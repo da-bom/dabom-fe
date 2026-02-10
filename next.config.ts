73fa6eeb4166bdf2330eae5fedbb21fd21923939
tray0244 <tray024400@gmail.com>
2026-02-10 15:07:20 +0900

    refactor: 확장자 검사할 때, 대소문자 구분하지 않기

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/,
+      test: /\.svg$/i,
       use: ['@svgr/webpack'],
     });
 
