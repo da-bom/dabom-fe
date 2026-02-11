f1ca7c1f3c5620e31343314fea2f0270dc7778d6
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-02-11 18:46:46 +0900

    DABOM-114 feat: 공통 박스 컴포넌트 구현

---
+++ packages/shared/src/components/SubBox.tsx
@@ -0,0 +1,22 @@
+import { cn } from "../utils/cn";
+
+const SubBox = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "bg-background-sub rounded-md border-[1px] border-gray-100",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default SubBox;
