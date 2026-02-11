ce0fb3b779cab415e465b61e4c09704c99160e0c
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-02-11 14:00:42 +0900

    feat: 어드민 라우팅 설정

---
+++ apps/admin/src/app/(afterLogin)/family/page.tsx
@@ -0,0 +1,5 @@
+const FamilyPage = () => {
+  return <div>family</div>;
+};
+
+export default FamilyPage;
---
+++ apps/admin/src/app/(afterLogin)/policy/page.tsx
@@ -0,0 +1,5 @@
+const PolicyPage = () => {
+  return <div>Policy</div>;
+};
+
+export default PolicyPage;
