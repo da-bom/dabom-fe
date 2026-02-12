ba9507a47da685e85953d11a0ee2bf64acf50ba8
tray0244 <tray024400@gmail.com>
2026-02-12 18:37:48 +0900

    feat: svg 추가

---
+++ packages/shared/src/assets/icons/Right.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgRight = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width={7} height={12} fill="none" {...props}><path fill="#101010" d="M0 1.073 1.062 0 7 6l-5.938 6L0 10.927 4.877 6z" /></svg>;
+export default SvgRight;
