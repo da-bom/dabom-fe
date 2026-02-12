199992a9f5fddc2203a16ae2969594c6c7eaa257
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-02-12 10:49:03 +0900

    DABOM-127 feat: 데이터 사용량 그래프 구현

--- apps/admin/src/components/family/FamilyDetail.tsx
+++ apps/admin/src/components/family/FamilyDetail.tsx
@@ -1,9 +1,12 @@
 import { Badge, Button } from "@repo/shared/src";
 import SubBox from "@repo/shared/src/components/SubBox";
 import { formatFileSize } from "@repo/shared/src/utils/fileSize";
+import dayjs from "dayjs";
 import { FAMILY_DETAIL } from "src/data/familyDetail";
 
 const FamilyDetail = () => {
+  const rate = (FAMILY_DETAIL.usedBytes / FAMILY_DETAIL.totalQuotaBytes) * 100;
+
   return (
     <div className="flex h-full flex-col justify-between p-4">
       <div className="flex items-center gap-2">
@@ -13,10 +16,24 @@ const FamilyDetail = () => {
         <span className="text-h2-d">가족 상세 관리</span>
       </div>
       <div className="w-full border-[1px] border-gray-100" />
-      <SubBox className="p-4">
+
+      <SubBox className="flex flex-col gap-4 p-4">
         <p className="text-body1-d flex justify-between">
           <span>총 사용량: {formatFileSize(FAMILY_DETAIL.usedBytes)}</span>
           <span>한도: {formatFileSize(FAMILY_DETAIL.totalQuotaBytes)}</span>
+        </p>
+        <div className="h-9 w-full rounded-full bg-gray-200">
+          <div
+            className="bg-primary h-full rounded-full"
+            style={{ width: `${rate}%` }}
+          />
+        </div>
+        <p className="flex justify-between">
+          <span className="text-body2-d text-gray-800">
+            업데이트 일시:{" "}
+            {dayjs(FAMILY_DETAIL.updatedAt).format("YYYY. MM. DD. HH:mm")}
+          </span>
+          <span className="text-h2-d text-primary">{rate}%</span>
         </p>
       </SubBox>
       <SubBox className="p-4">
