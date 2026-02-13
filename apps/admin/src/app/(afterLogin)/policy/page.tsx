a86a5a12fb1489f91ef40ec4159a5001c5eb43b7
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-02-13 15:22:21 +0900

    DABOM-139 feat: FilterBar 컴포넌트 구현

--- apps/admin/src/app/(afterLogin)/policy/page.tsx
+++ apps/admin/src/app/(afterLogin)/policy/page.tsx
@@ -1,5 +1,21 @@
+"use client";
+
+import { useState } from "react";
+
+import { FilterType } from "src/types/FilterType";
+
+import FilterBar from "@admin/components/policy/FilterBar";
+
 const PolicyPage = () => {
-  return <div>Policy</div>;
+  const [selectedFilter, setSelectedFilter] = useState<FilterType>("ALL");
+  return (
+    <div>
+      <FilterBar
+        selectedFilter={selectedFilter}
+        setSelectedFilter={setSelectedFilter}
+      />
+    </div>
+  );
 };
 
 export default PolicyPage;
---
+++ apps/admin/src/components/policy/FilterBar.tsx
@@ -0,0 +1,57 @@
+import { SubBox, cn } from "@shared";
+import { FilterType } from "src/types/FilterType";
+
+const FilterButton = ({
+  children,
+  isSelected,
+  onClick,
+}: {
+  children: React.ReactNode;
+  isSelected: boolean;
+  onClick: () => void;
+}) => {
+  return (
+    <button
+      className={cn(
+        "h-10 w-30 cursor-pointer rounded-lg",
+        isSelected ? "text-primary bg-brand-white" : "text-gray-600",
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+const FilterBar = ({
+  selectedFilter,
+  setSelectedFilter,
+}: {
+  selectedFilter: FilterType;
+  setSelectedFilter: (selectedFIlter: FilterType) => void;
+}) => {
+  return (
+    <SubBox className="w-fit rounded-2xl p-2">
+      <FilterButton
+        isSelected={selectedFilter === "ALL"}
+        onClick={() => setSelectedFilter("ALL")}
+      >
+        전체
+      </FilterButton>
+      <FilterButton
+        isSelected={selectedFilter === "ACTIVE"}
+        onClick={() => setSelectedFilter("ACTIVE")}
+      >
+        활성화
+      </FilterButton>
+      <FilterButton
+        isSelected={selectedFilter === "DEACTIVE"}
+        onClick={() => setSelectedFilter("DEACTIVE")}
+      >
+        비활성화
+      </FilterButton>
+    </SubBox>
+  );
+};
+
+export default FilterBar;
