07d962b5eec89f8f02d0e4fcb7be1265a752a4df
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-02-13 14:51:10 +0900

    DABOM-139 feat: 어드민 로그인 레이아웃 제거

---
+++ apps/admin/src/app/(afterLogin)/layout.tsx
@@ -0,0 +1,20 @@
+import "@globalstyles";
+
+import Header from "@admin/components/Header";
+import Sidebar from "@admin/components/Sidebar";
+
+export default function AfterLoginLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen w-full overflow-hidden">
+      <Sidebar />
+      <div className="flex w-full flex-col gap-8 p-5">
+        <Header />
+        {children}
+      </div>
+    </div>
+  );
+}
