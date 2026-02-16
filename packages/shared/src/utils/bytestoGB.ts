cab4d577c223cf3108787a7e36d43050c11c92e8
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-02-16 22:23:03 +0900

    DABOM-95 remove: bytestoGB 제거

--- packages/shared/src/utils/bytestoGB.ts
+++
@@ -1,6 +0,0 @@
-export const bytesToGB = (bytes: number) =>
-  Number((bytes / (1024 * 1024 * 1024)).toFixed(1));
-
-export const gbToBytes = (gb: number) => {
-  return gb * 1024 * 1024 * 1024;
-};
