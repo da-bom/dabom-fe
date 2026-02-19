6502fe9c7b7620749712365e9b563f2adedbc979
tray0244 <tray024400@gmail.com>
2026-02-19 17:57:53 +0900

    DABOM-190 refactor: 안쓰는 아이콘 삭제및 currentColor 지정

--- packages/shared/src/assets/icons/ListColor.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-import type { SVGProps } from "react";
-const SvgListColor = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width={10} height={5} fill="none" {...props}><path fill="#FD2E97" d="M0 3h1.111V2H0zm0 2h1.111V4H0zm0-4h1.111V0H0zm2.222 2H10V2H2.222zm0 2H10V4H2.222zm0-5v1H10V0zM0 3h1.111V2H0zm0 2h1.111V4H0zm0-4h1.111V0H0zm2.222 2H10V2H2.222zm0 2H10V4H2.222zm0-5v1H10V0z" /></svg>;
-export default SvgListColor;
