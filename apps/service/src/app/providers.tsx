807935a295a9f2743acb25dc35961e96df080c2c
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-03-12 19:33:25 +0900

    DABOM-393 feat: 디자인 수정

--- apps/service/src/app/providers.tsx
+++ apps/service/src/app/providers.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export function Providers({
   children,
@@ -11,5 +12,10 @@ export function Providers({
 }>) {
   const [queryClient] = useState(() => new QueryClient());
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-left" />
+    </QueryClientProvider>
+  );
 }
