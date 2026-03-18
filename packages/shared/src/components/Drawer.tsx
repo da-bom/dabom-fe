f2667db6c24e7df240416862eedfb80cf809c4ae
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-03-19 00:00:50 +0900

    DABOM-476 feat: Drawer 롤백

--- packages/shared/src/components/Drawer.tsx
+++ packages/shared/src/components/Drawer.tsx
@@ -1,23 +1,13 @@
 'use client';
 
-import { useState } from 'react';
-
 import { useRouter } from 'next/navigation';
 
 import { ChevronIcon } from '../assets/icons';
-import ConfirmModal from './CloseConfirmModal';
 
 const Drawer = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
 
-  const [showConfirm, setShowConfirm] = useState(false);
-
-  const handleCloseAttempt = () => {
-    setShowConfirm(true);
-  };
-
-  const confirmClose = () => {
-    setShowConfirm(false);
+  const handleClose = () => {
     router.back();
   };
 
@@ -25,7 +15,7 @@ const Drawer = ({ children }: { children: React.ReactNode }) => {
     <>
       <div
         role="presentation"
-        onClick={handleCloseAttempt}
+        onClick={handleClose}
         className="fixed inset-0 z-50 flex h-screen justify-end bg-black/20"
       >
         <aside
@@ -33,19 +23,13 @@ const Drawer = ({ children }: { children: React.ReactNode }) => {
           className="bg-brand-white flex h-full w-175 flex-col border-l border-gray-300 px-11 py-8 shadow-[-4px_0_10px_rgba(0,0,0,0.1)]"
         >
           <div className="flex h-full flex-col gap-10 overflow-y-auto">
-            <button className="text-brand-dark w-fit cursor-pointer" onClick={handleCloseAttempt}>
+            <button className="text-brand-dark w-fit cursor-pointer" onClick={handleClose}>
               <ChevronIcon sx={{ width: 20 }} />
             </button>
             {children}
           </div>
         </aside>
       </div>
-
-      <ConfirmModal
-        showConfirm={showConfirm}
-        onClose={() => setShowConfirm(false)}
-        onConfirm={confirmClose}
-      />
     </>
   );
 };
